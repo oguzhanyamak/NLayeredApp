82c16da6e2c3e763d29079e36aa0fffb1dabf552
Oğuzhan Yamak <oguzhanyamak@outlook.com>
2021-11-02 15:44:36 +0300

    Fluent Validation

--- Northwind.Business/Concrete/ProductManager.cs
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -1,4 +1,7 @@
-﻿using Northwind.Business.Abstract;
+﻿using FluentValidation;
+using Northwind.Business.Abstract;
+using Northwind.Business.Utilities;
+using Northwind.Business.ValidationRules.FluentValidation;
 using Northwind.DataAccess.Abstract;
 using Northwind.DataAccess.Concrete;
 using Northwind.DataAccess.Concrete.EntityFramework;
@@ -24,6 +27,7 @@ namespace Northwind.Business.Concrete
 
         public void Add(Product product)
         {
+            ValidationTool.Validate(new ProductValidator(), product);
             _productDal.Add(product);
         }
 
